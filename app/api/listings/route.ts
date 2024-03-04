274890fdfe8e5c44f8b8c6c4f30b43d07baf6c21
Tuan Nguyen <tuan.nat915@gmail.com>
2024-03-04 00:34:49 -0500

    created api for create listings

---
+++ app/api/listings/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server"
+import prisma from "@/libs/prismadb"
+import { getCurrentUser } from "@/actions/getCurrentUser"
+
+export const POST = async (request: Request) => {
+  const currentUser = await getCurrentUser()
+  if (!currentUser) {
+    return NextResponse.error()
+  }
+
+  const body = await request.json()
+  const {
+    title,
+    description,
+    imageSrc,
+    category,
+    roomCount,
+    bathroomCount,
+    guestCount,
+    location,
+    price,
+  } = body
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error()
+    }
+  })
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      price: parseInt(price),
+      locationValue: location.value,
+      userId: currentUser.id,
+    },
+  })
+  return NextResponse.json(listing)
+}
--- components/modal/RentModal.tsx
+++ components/modal/RentModal.tsx
@@ -5,11 +5,15 @@ import { useMemo, useState } from "react"
 import Heading from "../Heading"
 import { categories } from "@/constants"
 import CategoryInput from "../inputs/CategoryInput"
-import { FieldValues, useForm } from "react-hook-form"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
 import CountrySelect from "../inputs/CountrySelect"
 import dynamic from "next/dynamic"
 import Counter from "../inputs/Counter"
 import ImageUpload from "../inputs/ImageUpload"
+import Input from "../inputs/Input"
+import axios from "axios"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
 
 enum STEPS {
   CATEGORY = 0,
@@ -20,9 +24,10 @@ enum STEPS {
   PRICE = 5,
 }
 const RentModal = () => {
-  const [step, setStep] = useState(STEPS.CATEGORY)
+  const router = useRouter()
   const rentModal = useRentModal()
-
+  const [step, setStep] = useState(STEPS.CATEGORY)
+  const [isLoading, setIsLoading] = useState(false)
   //**************FORM ******************/
   const {
     register,
@@ -73,6 +78,26 @@ const RentModal = () => {
   }
   const onNext = () => {
     setStep(step + 1)
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext()
+    }
+    setIsLoading(true)
+    axios
+      .post(`/api/listings`, data)
+      .then(() => {
+        toast.success("Listing Created!!!")
+        router.refresh()
+        reset()
+        setStep(STEPS.CATEGORY)
+        rentModal.onClose()
+      })
+      .catch(() => {
+        toast.error("Something went wrong")
+      })
+      .finally(() => setIsLoading(false))
   }
 
   const actionLabel = useMemo(() => {
@@ -171,6 +196,53 @@ const RentModal = () => {
         />
       </div>
     )
+  } else if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best!"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disable={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disable={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night"
+        />
+        <Input
+          formatPrice
+          id="price"
+          label="Price"
+          type="number"
+          disable={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
   }
 
   return (
@@ -178,7 +250,7 @@ const RentModal = () => {
       <Modal
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={onNext}
+        onSubmit={handleSubmit(onSubmit)}
         title="Airbnb your home"
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
